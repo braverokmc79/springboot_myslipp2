e7cc836af20c5632761b80bab2fd31f826732af4
braverokmc79 <braverokmc79@gmail.com>
2018-09-12 16:53:08 +0900

    11. 사용자 목록 기능 구현

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -1,8 +1,12 @@
 package net.slipp.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -10,12 +14,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UserController {
 	
 	private static final Logger log = LoggerFactory.getLogger(UserController.class);
-
+	
+	private List<User> users=new ArrayList<User>();
+	
 	@PostMapping("/create")
 	public String create(User user) {
 		log.info("userId : " +user.toString());
-		return "index";
+		users.add(user);
+		return "redirect:/list";
 	}
 
+	@GetMapping("/list")
+	public String list(Model model){
+		model.addAttribute("users", users);
+		return "list";
+	}
 
+	
+	
 }
