1dbadbfe8d48cc29a37e31c18a12d9593062879d
braverokmc79 <braverokmc79@gmail.com>
2018-09-15 18:28:41 +0900

    새로운 HTML 적용 및 URL 리팩토링

---
+++ src/main/java/net/slipp/domain/UserRepository.java
@@ -0,0 +1,7 @@
+package net.slipp.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+//User id 타입import spock.util.mop.Use;
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+}
--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -5,28 +5,34 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import net.slipp.domain.User;
+import net.slipp.domain.UserRepository;
+
 @Controller
 public class UserController {
-	
 	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+	//private List<User> users=new ArrayList<User>();
 	
-	private List<User> users=new ArrayList<User>();
+	@Autowired
+	private UserRepository UserRepository;
 	
 	@PostMapping("/create")
 	public String create(User user) {
 		log.info("userId : " +user.toString());
-		users.add(user);
+		//users.add(user);
+		UserRepository.save(user);
 		return "redirect:/list";
 	}
 
 	@GetMapping("/list")
 	public String list(Model model){
-		model.addAttribute("users", users);
+		model.addAttribute("users", UserRepository.findAll());
 		return "list";
 	}
 
