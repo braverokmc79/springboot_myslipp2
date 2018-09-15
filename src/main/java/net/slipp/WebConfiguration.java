16ae2736a3e15e6232ea5522ea5836ce95273101
braverokmc79 <braverokmc79@gmail.com>
2018-09-15 20:39:25 +0900

    WebConfiguration
    @Configuration 변경

--- src/main/java/net/slipp/WebConfiguration.java
+++ src/main/java/net/slipp/WebConfiguration.java
@@ -4,8 +4,9 @@ import org.h2.server.web.WebServlet;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
 public class WebConfiguration {
 		
 	  @Bean
