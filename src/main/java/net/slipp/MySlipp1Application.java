79344d1446c795e0854058b9ddcb6985f0b20e39
braverokmc79 <braverokmc79@gmail.com>
2018-10-01 14:04:50 +0900

    28-2 질문 목록 기능 구현

--- src/main/java/net/slipp/MySlipp1Application.java
+++ src/main/java/net/slipp/MySlipp1Application.java
@@ -2,12 +2,7 @@ package net.slipp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-
-import org.springframework.context.annotation.ComponentScan;
-
-import net.slipp.web.WelcomeController;
 
 @SpringBootApplication
 public class MySlipp1Application extends SpringBootServletInitializer {
